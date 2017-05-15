c63b614cbea381338d006cdeeb01a765e348da6b
sumeetv21 <sumwin21@gmail.com>
2017-05-15 17:44:50 +0530

    Add files via upload

---
+++ Autobackup_Postgres.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+hostname=IP Address
+username=Database userName
+path="path/for/where/backup/stored "
+items=( "a" "b" "c" "d" "e" ) #databse name
+for i in "${items[@]}"
+do
+pg_dump -h $hostname -Fc -o -U $username "$i" > $path/"$i"_$(date +\%d-\%m-\%y-\%H-\%M-\%S).sql.gz		
+done
+chmod -R 0600 $path
